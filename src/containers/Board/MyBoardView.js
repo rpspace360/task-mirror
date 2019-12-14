0f3929a9de571e041b2ecc6de9150d855a80be0c
rpsingh <ramprakash@codekraft.in>
2019-12-14 16:48:48 +0530

    adds feature to drop card from one list to another

--- src/containers/Board/MyBoardView.js
+++ src/containers/Board/MyBoardView.js
@@ -1,256 +1,286 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import * as actions from "../../store/actions";
-import { Row, Col,  Button } from 'antd';
+import { Row, Col, Button } from "antd";
 
-import NewListOrCard  from "../NewListOrCard/NewListOrCard";
-
-
+import NewListOrCard from "../NewListOrCard/NewListOrCard";
 
 const reorderArray = (list, startIndex, endIndex) => {
-    const futureArray = Array.from(list);
-    const [removed] = futureArray.splice(startIndex, 1);
-    futureArray.splice(endIndex, 0, removed);
+  const futureArray = Array.from(list);
+  const [removed] = futureArray.splice(startIndex, 1);
+  futureArray.splice(endIndex, 0, removed);
 
-    return futureArray;
+  return futureArray;
 };
-
-
 
 const grid = 10;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
 
-    userSelect: 'none',
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
+  background: isDragging ? "lightgreen" : "white",
 
-    background: isDragging ? 'lightgreen' : 'white',
-
-    ...draggableStyle
+  ...draggableStyle
 });
 
 const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? 'lightblue' : 'lightgreen',
-    padding: grid,
-    
-    display: 'flex',
-   
-  
-  overflow: 'auto',
+  background: isDraggingOver ? "lightblue" : "lightgreen",
+  padding: grid,
+
+  display: "flex",
+
+  overflow: "auto"
 });
 
 const getCildListStyle = isDraggingOver => ({
-    background: isDraggingOver ? 'lightblue' : 'lightgrey',
-    padding: grid,
-    width: 150,
-    border: 'solid',
-    
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 150,
+  border: "solid"
 });
 
 const move = (source, destination, droppableSource, droppableDestination) => {
-    const sourceClone = Array.from(source);
-    const destClone = Array.from(destination);
-    const [removed] = sourceClone.splice(droppableSource.index, 1);
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
 
-    destClone.splice(droppableDestination.index, 0, removed);
+  destClone.splice(droppableDestination.index, 0, removed);
 
-    const result = {};
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destClone;
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
 
-    return result;
+  return result;
 };
 
 class MyBoardView extends Component {
-    state = {
-        data: [{
-            id: "1c9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
-            listTilte: "TODO",
-            cards: [{
-                id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4eed",
-                cardTitle: "Book Hotel",
-                cardDes: "Get a Hoetl",
-
-            },
-            {
-                id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfdbd4eed",
-                cardTitle: "Book Bus",
-                cardDes: "Get a Bus",
-            }]
-        },
-        {
-            id: "1b7b6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
-            listTilte: "PROGRESS",
-            cards: [{
-                id: "1b8d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4eed",
-                cardTitle: "Meeting",
-                cardDes: "Get a Meeting",
-
-            },
-            {
-                id: "1b9d7bcd-bbfd-4b2d-9b5d-ab8dfdbd4eed",
-                cardTitle: "Book Bus",
-                cardDes: "Get a Bus",
-            }]
-        }
-
-
-
+  state = {
+    data: [
+      {
+        id: "1c9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
+        listTilte: "TODO",
+        cards: [
+          {
+            id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4eed",
+            cardTitle: "Book Hotel",
+            cardDes: "Get a Hoetl"
+          },
+          {
+            id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfdbd4eed",
+            cardTitle: "Book Bus",
+            cardDes: "Get a Bus"
+          }
         ]
-    };
+      },
+      {
+        id: "1b7b6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
+        listTilte: "PROGRESS",
+        cards: [
+          {
+            id: "1b8d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4eed",
+            cardTitle: "Meeting",
+            cardDes: "Get a Meeting"
+          },
+          {
+            id: "1b9d7bcd-bbfd-4b2d-9b5d-ab8dfdbd4eed",
+            cardTitle: "Book Bus",
+            cardDes: "Get a Bus"
+          }
+        ]
+      }
+    ]
+  };
 
+  getCardsArrayOfAList = id =>
+    this.props.boardData.find(element => element.id === id);
 
+  onDragListEnd = result => {
+    window.console.log(result, "ada");
+    const { source, destination } = result;
 
-    getList = id => this.state[this.id2List[id]];
-
-    getCardsArrayOfAList = id => this.props.boardData.find(element => element.id === id);
-
-    onDragEnd = result => {
-        window.console.log(result, "ada")
-        const { source, destination } = result;
-
-        if (!destination) {
-            return;
-        }
-
-        if (source.droppableId === destination.droppableId) {
-            const items = reorderArray(
-                this.props.boardData,
-                source.index,
-                destination.index
-            );
-
-
-
-            this.props.reorderCurrList(items);
-        } else {
-
-        }
-    };
-
-    onCardDragEnd = result => {
-        const { source, destination } = result;
-        window.console.log(result)
-
-        // dropped outside context area
-        if (!destination) {
-            return;
-        }
-
-        if (source.droppableId === destination.droppableId) {
-            window.console.log(this.getCardsArrayOfAList(source.droppableId))
-            let targetList = this.getCardsArrayOfAList(source.droppableId)
-            const cardsArray = reorderArray(
-                this.getCardsArrayOfAList(source.droppableId) ? this.getCardsArrayOfAList(source.droppableId).cards : [],
-                source.index,
-                destination.index
-            );
-            let reorderedList = { ...targetList, cards: cardsArray }
-
-
-            this.props.reorderCurrList(this.props.boardData.map(item => item.id === source.droppableId ? reorderedList
-                : item));
-        } else {
-
-        }
-    };
-
-    render() {
-        return (
-            <React.Fragment>
-            <Row>
-            <Col span={20}>
-            <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppable" direction="horizontal" >
-                    {(provided, snapshot) => (
-                        <div
-                            ref={provided.innerRef}
-                            style={getListStyle(snapshot.isDraggingOver)}>
-                            {this.props.boardData.map((item, index) => (
-                                <Draggable
-                                    key={item.id}
-                                    draggableId={item.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            style={getItemStyle(
-                                                snapshot.isDragging,
-                                                provided.draggableProps.style
-                                            )}>
-                                            {item.listTilte}
-                                            <DragDropContext onDragEnd={this.onCardDragEnd}>
-                                                <Droppable droppableId={item.id} >
-                                                    {(childProvided, childSnapshot) => (
-                                                        <div
-                                                            ref={childProvided.innerRef}
-                                                            style={getCildListStyle(childSnapshot.isDraggingOver)}>
-                                                            {item.cards.map((childItem, childIndex) => (
-                                                                <Draggable
-                                                                    key={childItem.id}
-                                                                    draggableId={childItem.id}
-                                                                    index={childIndex}>
-                                                                    {(childProvided, childSnapshot) => (
-                                                                        <div
-                                                                            ref={childProvided.innerRef}
-                                                                            {...childProvided.draggableProps}
-                                                                            {...childProvided.dragHandleProps}
-                                                                            style={getItemStyle(
-                                                                                childSnapshot.isDragging,
-                                                                                childProvided.draggableProps.style
-                                                                            )}>
-                                                                            {childItem.cardTitle}
-                                                                        </div>
-                                                                    )}
-                                                                </Draggable>
-                                                            ))}
-                                                            {provided.placeholder}
-                                                        </div>
-                                                    )}
-                                                </Droppable>
-                                            </DragDropContext>
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-
-            </DragDropContext>
-            </Col>
-            <Col span={4}>
-            <NewListOrCard itemType="List"></NewListOrCard>
-            
-            </Col>
-          </Row>
-            
-            </React.Fragment>
-        );
+    if (!destination) {
+      return;
     }
+
+    if (source.droppableId === destination.droppableId) {
+      const items = reorderArray(
+        this.props.boardData,
+        source.index,
+        destination.index
+      );
+      this.props.reorderCurrList(items);
+    } else {
+    }
+  };
+  onDragEndHandler = result => {
+    window.console.log(result, "arrr");
+    const { destination, type } = result;
+    if (!destination) {
+      return;
+    }
+    if (type === "CARD") {
+      this.onCardDragEnd(result);
+    } else if (type === "LIST") {
+      this.onDragListEnd(result);
+    }
+  };
+
+  onCardDragEnd = result => {
+    const { source, destination } = result;
+    window.console.log(result);
+
+    // dropped outside context area
+    if (!destination) {
+      return;
+    }
+
+    if (source.droppableId === destination.droppableId) {
+      window.console.log(this.getCardsArrayOfAList(source.droppableId));
+      let targetList = this.getCardsArrayOfAList(source.droppableId);
+      const cardsArray = reorderArray(
+        this.getCardsArrayOfAList(source.droppableId)
+          ? this.getCardsArrayOfAList(source.droppableId).cards
+          : [],
+        source.index,
+        destination.index
+      );
+      let reorderedList = { ...targetList, cards: cardsArray };
+
+      this.props.reorderCurrList(
+        this.props.boardData.map(item =>
+          item.id === source.droppableId ? reorderedList : item
+        )
+      );
+    } else {
+      let targetList = this.getCardsArrayOfAList(source.droppableId);
+      let destinationList = this.getCardsArrayOfAList(destination.droppableId);
+      const newResult = move(
+        targetList.cards,
+        destinationList.cards,
+        source,
+        destination
+      );
+      let reorderedList1 = {
+        ...targetList,
+        cards: newResult[`${source.droppableId}`]
+      };
+      let reorderedList2 = {
+        ...destinationList,
+        cards: newResult[`${destination.droppableId}`]
+      };
+
+      this.props.reorderCurrList(
+        this.props.boardData.map(item => {
+          if (item.id === source.droppableId) return reorderedList1;
+          if (item.id === destination.droppableId) return reorderedList2;
+          return item;
+        })
+      );
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Row>
+          <Col span={20}>
+            <DragDropContext onDragEnd={this.onDragEndHandler}>
+              <Droppable
+                droppableId="droppable"
+                direction="horizontal"
+                type="LIST"
+              >
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    style={getListStyle(snapshot.isDraggingOver)}
+                  >
+                    {this.props.boardData.map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.id}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}
+                          >
+                            {item.listTilte}
+                            {/* <DragDropContext onDragEnd={this.onCardDragEnd}> */}
+                            <Droppable droppableId={item.id} type="CARD">
+                              {(childProvided, childSnapshot) => (
+                                <div
+                                  ref={childProvided.innerRef}
+                                  style={getCildListStyle(
+                                    childSnapshot.isDraggingOver
+                                  )}
+                                >
+                                  {item.cards.map((childItem, childIndex) => (
+                                    <Draggable
+                                      key={childItem.id}
+                                      draggableId={childItem.id}
+                                      index={childIndex}
+                                    >
+                                      {(childProvided, childSnapshot) => (
+                                        <div
+                                          ref={childProvided.innerRef}
+                                          {...childProvided.draggableProps}
+                                          {...childProvided.dragHandleProps}
+                                          style={getItemStyle(
+                                            childSnapshot.isDragging,
+                                            childProvided.draggableProps.style
+                                          )}
+                                        >
+                                          {childItem.cardTitle}
+                                        </div>
+                                      )}
+                                    </Draggable>
+                                  ))}
+                                  {provided.placeholder}
+                                </div>
+                              )}
+                            </Droppable>
+                            {/* </DragDropContext> */}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </Col>
+          <Col span={4}>
+            <NewListOrCard itemType="List"></NewListOrCard>
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        boardData: state.board.data
-
-    };
+  return {
+    boardData: state.board.data
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        reorderCurrList: (boardData) => dispatch(actions.reorderCurrList(boardData)),
-
-    };
+  return {
+    reorderCurrList: boardData => dispatch(actions.reorderCurrList(boardData))
+  };
 };
 
-MyBoardView.propTypes = {
-};
+MyBoardView.propTypes = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyBoardView);
-
--- src/containers/MainComponent/MainComponent.js
+++ src/containers/MainComponent/MainComponent.js
@@ -1,72 +1,54 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Layout } from "antd";
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 import MyBoardView from "../Board/MyBoardView";
 
-
 import HorizontalPane from "../../components/TaskMirror/HorizontalPane/HorizontalPane";
 
-
-
 import "./MainComponent.scss";
-
 
 const { Header, Content, Footer, Sider } = Layout;
 
 class MainComponent extends Component {
+  render() {
+    if (true) {
+      return this.renderWhenLoggedIn();
+    }
+  }
 
+  renderWhenLoggedIn() {
+    return (
+      <React.Fragment>
+        <Layout>
+          <HorizontalPane />
 
+          <Layout>
+            <Content style={{ backgroundColor: "lightgreen", height: "600px" }}>
+              <Switch>
+                <Redirect exact from="/" to="/board/" />
+                <Redirect exact from="/login" to="/board/" />
 
-	render() {
-		if (true) {
-			return this.renderWhenLoggedIn();
-		}
-	}
-
-	renderWhenLoggedIn() {
-		return (
-			<React.Fragment>
-				<Layout>
-					
-						<HorizontalPane />
-						
-					
-					<Layout>
-
-						<Content style={{ backgroundColor: 'lightgreen', height: "600px" }}>
-							<Switch>
-								<Redirect exact from="/" to="/board/" />
-								<Redirect exact from="/login" to="/board/" />
-
-								<Route path="/board/" exact component={MyBoardView} />
-
-							</Switch>
-						</Content>
-
-					</Layout>
-				</Layout>
-			</React.Fragment>
-		);
-	}
+                <Route path="/board/" exact component={MyBoardView} />
+              </Switch>
+            </Content>
+          </Layout>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-	};
+  return {};
 };
 
 const mapDispatchToProps = dispatch => {
-	return {
-
-	};
+  return {};
 };
 
-MainComponent.propTypes = {
-
-};
+MainComponent.propTypes = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainComponent);
