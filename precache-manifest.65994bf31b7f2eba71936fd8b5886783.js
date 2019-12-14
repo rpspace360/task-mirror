ec1c58fc613c61efd180a37f49565cb3db654328
rpsingh <ramprakash@codekraft.in>
2019-12-14 20:13:11 +0530

    Updates

---
+++ precache-manifest.65994bf31b7f2eba71936fd8b5886783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed2ae3052137c0d67da0b689fbe8e96",
+    "url": "/task-mirror/index.html"
+  },
+  {
+    "revision": "03def349db2b8dd81f94",
+    "url": "/task-mirror/static/css/2.cf5c3ece.chunk.css"
+  },
+  {
+    "revision": "b68a483ae1732bd9a927",
+    "url": "/task-mirror/static/css/main.1c5273fc.chunk.css"
+  },
+  {
+    "revision": "03def349db2b8dd81f94",
+    "url": "/task-mirror/static/js/2.f50b6208.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/task-mirror/static/js/2.f50b6208.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b68a483ae1732bd9a927",
+    "url": "/task-mirror/static/js/main.5718ee6f.chunk.js"
+  },
+  {
+    "revision": "ece9026be2297c619968",
+    "url": "/task-mirror/static/js/runtime-main.b821a83f.js"
+  }
+]);
